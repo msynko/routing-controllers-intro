b9830073709c8d582b17fbb7fab1a36af3e1f9c5
msynko <marina.synko@gmail.com>
2018-12-04 19:32:19 -0500

    commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,44 @@
+class PagesController < ApplicationController
+
+before_action :set_kitten_url, only: [:kitten, :kittens]
+
+  def welcome
+    @header = "Welcome"
+  end
+
+  def about
+    @header = "About Us"
+  end
+
+  def kitten
+  end
+
+  def kittens
+  end
+
+  def set_kitten_url
+    requested_size = params[:size]
+    @kitten_url = "http://placekitten.com/#{requested_size}/#{requested_size}"
+  end
+
+  def contest
+  flash[:notice] = "Sorry, the contest has ended"
+  redirect_to "/welcome"
+end
+
+def secrets
+  if params[:magic_word] == "hazelnut"
+        render :secrets
+    else
+        redirect_to '/intruder'
+  end
+end
+
+def intruder
+    flash[:alert] = "Sorry, you're not authorized to see that page!"
+    
+    @header = "access denied!"
+  end
+
+
+end
